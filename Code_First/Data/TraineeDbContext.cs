25e655f18a331b53cd05c349ed254a6d90230034
Karthik707 <karthikkulal707@gmail.com>
2022-10-14 16:58:54 +0530

    Add project files.

---
+++ Code_First/Data/TraineeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Code_First.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Code_First.Data
+{
+    public class TraineeDbContext:DbContext
+    {
+        public TraineeDbContext(DbContextOptions<TraineeDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Trainee> Trainees { get; set; }
+    }
+}
---
+++ Code_First/Models/Trainee.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Code_First.Models
+{
+    public class Trainee
+    {
+        [Key]
+        public int TraineeID { get; set; }
+
+        [Required(ErrorMessage = "Enter Name")]
+        
+        public string? TraineeName { get; set; }
+
+        [Range(21, 58, ErrorMessage = "Enter age between 21 to 58")]
+        public int TraineeAge { get; set; }
+
+        public DateTime TraineeDOJ { get; set; }
+        public DateTime TraineeDOB { get; set; }
+
+        
+        public string? TraineeMobile { get; set; }
+
+       
+        public string? TraineeEmail { get; set; }
+
+        
+        [Required(ErrorMessage = "Enter the Password")]
+        public string? TraineePassword { get; set; }
+
+        
+        [Required(ErrorMessage = "Enter the Confirm Password")]
+        [Compare("TraineePassword", ErrorMessage = "Password does not match")]
+        public string? TraineeConfirmPassword { get; set; }
+    }
+}
